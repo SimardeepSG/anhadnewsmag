2828623df8623a2be3973d70aa8336c63eee849b
SimardeepSG <simardeepsgawra@gmail.com>
2021-07-28 15:39:33 -0700

    events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
 const  Home = () => {
+
+
+    const handleClick = (e) => {
+        console.log('sdafasdfasdf', e)
+    }
+    const handleClickAgain = (name) => {
+        console.log(name)
+    }
+
     return ( 
         <div className="home">
             <h2>
                 Homepage
             </h2>
+            <button onClick={handleClick}> CLick me </button>
+            <button onClick= {() => {
+                handleClickAgain ('mario')
+            }}> CLick me </button>
         </div>
      );
 }
