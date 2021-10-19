6021a2741ac5c90ddaf0892affb832a5e7b12eb7
“Simardeep <simardeepsgawra@gmail.com>
2021-10-19 01:19:22 -0700

    Front Page improvements - background, navbar, reszing - still neds work though

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,81 +1,56 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Sample from './Sample';
-import Staff from './Staff';
-import Contact from './Contact';
-import About from './About';
-import Donate from './Donate';
-import Footer from './Footer';
-import MailingForm from './MailingForm';
-
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Sample from "./Sample";
+import Staff from "./Staff";
+import Contact from "./Contact";
+import About from "./About";
+import Donate from "./Donate";
+import Footer from "./Footer";
 
 function App() {
   return (
-    
     <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* removed  <h1>Anhad|ਅਨਹਦ </h1> from here */}
 
-
-    <div className="App">
-      
-      <Navbar />                         
-      <div className="content">
-        {/* removed  <h1>Anhad|ਅਨਹਦ </h1> from here */}
-
-
-        <Switch>                          
-            <Route exact path="/"> 
+          <Switch>
+            <Route exact path="/">
               <Home />
             </Route>
 
-            <Route path="/sample"> 
+            <Route path="/sample">
               <Sample />
             </Route>
-  
-            <Route path="/staff"> 
+
+            <Route path="/staff">
               <Staff />
             </Route>
 
-            <Route path="/about"> 
+            <Route path="/about">
               <About />
             </Route>
 
-
-            <Route path="/contact"> 
+            <Route path="/contact">
               <Contact />
             </Route>
 
-
-            <Route path="/Donate"> 
+            <Route path="/Donate">
               <Donate />
             </Route>
 
-            <Route path="/Subscribe"> 
-            </Route>
-          
-            <Route path="/MailingForm" > 
-              <body className = "bodyofmail">
-              <MailingForm />
-              </body>
-              
-            </Route>
-        
-            
+            <Route path="/Subscribe"></Route>
 
-        </Switch>
-      
-        
+            <Route path="/MailingForm"></Route>
+          </Switch>
+        </div>
       </div>
-    </div> 
-  
 
-      <Footer /> 
-   
-
+      <Footer />
     </Router>
-     
   );
-  
 }
 
 export default App;
--- Client/src/Home.js
+++ Client/src/Home.js
@@ -1,33 +1,50 @@
-const  Home = () => {
+import React, { Component } from "react";
+import "./styles/homeAndMail.css";
+import Subscribe from "./ComingSoon/subscribe";
 
-//     // these buttons are not needed
-// /*
-//     const handleClick = (e) => {
-//         console.log('sdafasdfasdf', e)
-//     }
-//     const handleClickAgain = (name) => {
-//         console.log(name)
-//     }
+class Home extends Component {
+  //     // these buttons are not needed
+  // /*
+  //     const handleClick = (e) => {
+  //         console.log('sdafasdfasdf', e)
+  //     }
+  //     const handleClickAgain = (name) => {
+  //         console.log(name)
+  //     }
 
-// */
+  // */
 
-    return ( 
-        <div className="home">
+  state = {
+    subscribe: {
+      placeholder: "Enter Email Address",
+      buttonText: "Submit",
+    },
+  };
 
-            <div className="home-page-div">
-          <h1>ANHAD</h1>
-          <p>a student-run sikh sprint and multimedia magazine</p>
-          <p>inspiring a new wave of sikh literacy</p>
-           <p>and connectedness to our guru</p>
-           
-        </div>
+  render() {
+    const { subscribe } = this.state;
 
-            
-
-
-        </div>
-     );
+    // want to destructure state because we dont wanna do this.state. ... each time so we can just do countdown and all
+    return (
+      <div className="home">
+        <body className="bodyofmail">
+          <div className="home-page-div">
+            <h2> Anhad Magazine</h2>
+            <h1>Anhd</h1>
+            <p>a student-run sikh sprint and multimedia magazine</p>
+            <p>inspiring a new wave of sikh literacy</p>
+            <p>and connectedness to our guru</p>
+            <div className="blankspace"></div>
+            <Subscribe
+              placeholder={subscribe.placeholder}
+              buttonText={subscribe.buttonText}
+            ></Subscribe>
+          </div>
+        </body>
+        <div className="arrow">╲╱</div>
+      </div>
+    );
+  }
 }
 
- 
 export default Home;
