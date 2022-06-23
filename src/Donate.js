16f4646f25304ff759a8bb58e8905e90a839fa0f
InderjeetSG1 <inderjeetgawra@gmail.com>
2022-06-22 17:15:18 -0700

    Update 1.2 -> staff page

--- src/Donate.js
+++ src/Donate.js
@@ -1,10 +1,19 @@
 import "./styles/donate.css";
 
+function sayHello() {
+  alert('You clicked me!');
+}
+
 const Donate = () => {
   return (
     <div className="donateContainer">
+      <a href="https://react.school" target="_blank">
+        <button> Link Button </button>
+      </a>
       {/* ---------------------------------------- */}
+      
       <div className="donateTitle">DONATE</div>
+      <div className="donateHeader">Your Donation will provide...</div>
       <div className="donateDescription">
         Anhad Magazine appreciates your support in any way possible. For us to
         succeed in expanding our work and ensuring it is long-lasting, we need
@@ -21,40 +30,47 @@ const Donate = () => {
         {/* ---------------------------------------- */}
         <div className="donateCostMenu">
           <div className="donateItemTitle">
-            Anhad Magazine <br></br>Costs Menu
+            {/* Anhad Magazine <br></br>Costs Menu */}
           </div>
           <div className="donateItemChart">
             <div className="donateItemItem">
-              <div>ITEM</div>
-              <p>01. ONE PRINTED MAGAZINE </p>
+              {/* <div>ITEM</div> */}
+              {/* <p>01. ONE PRINTED MAGAZINE </p>
               <p>02. ONE PRINTED MAGAZINE + DOMESTIC SHIPPING </p>
               <p>
                 03. ONE PRINTED MAGAZINE + INTERNATIONAL SHIPPING <br></br>
                 [PRICE DEPENDS ON COUNTRY SELECTION]
               </p>
-              <p>04. PACKAGING COSTS PER INDIVIDUAL </p>
+              <p>04. PACKAGING COSTS PER INDIVIDUAL </p> */}
             </div>
 
             <div className="donateItemCost">
-              <div>COST OF PRODUCTION </div>
-              <p>$5.00</p>
+              {/* <div>COST OF PRODUCTION </div> */}
+              {/* <p>$5.00</p>
               <p>$8.00 +</p>
               <p>$20.00 +</p>
-              <p>$5.00</p>
+              <p>$5.00</p> */}
             </div>
           </div>
         </div>
         {/* ---------------------------------------- */}
         <div className="donateColumn2">
-          <div className="venmo">VENMO US! @anhadmagazine</div>
+          {/* { <div className="venmo">VENMO US! @anhadmagazine</div>} */}
           {/* ---------------------------------------- */}
-          <div className="dasvandh">Donate via Dasvandh Network! </div>
+          {/* { <div className="dasvandh">Donate via Dasvandh Network! </div>} */}
           {/* ---------------------------------------- */}
-          <div className="policy">Donor Policy and FAQs</div>
+          {/* {<div className="policy">Donor Policy and FAQs</div> } */}
         </div>
         {/* ---------------------------------------- */}
       </div>
       {/* -------------------------------------------------------------------------------- */}
+      <a
+        href="https://docs.google.com/forms/u/0/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button> SUBMIT A REVIEW </button>
+      </a>
     </div>
   );
 };
