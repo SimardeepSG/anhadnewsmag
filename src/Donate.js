274ba87889f7b849dc60324b0c500acddbd88711
InderjeetSG1 <inderjeetgawra@gmail.com>
2022-06-28 16:37:36 -0700

    Donate Page - Added the First Part of Page

--- src/Donate.js
+++ src/Donate.js
@@ -3,70 +3,50 @@ import "./styles/donate.css";
 const Donate = () => {
   return (
     <div className="donateContainer">
-      <a href="https://react.school" >
-        <button> Link Button </button>
-      </a>
-      {/* ---------------------------------------- */}
+{/* ---------------------------------------- */}
 
-      <div className="donateTitle">DONATE</div>
-      <div className="donateHeader">Your Donation will provide...</div>
-      <div className="donateDescription">
-        Anhad Magazine appreciates your support in any way possible. For us to
-        succeed in expanding our work and ensuring it is long-lasting, we need
-        your support. Your donation to Anhad will ensure Sikh literacy for ages
-        with guaranteed permanence and accessibility to the Sikh diaspora
-        through language and ownership of the magazine. All donations go
-        strictly toward: (1) promotion of the magazine, (2) printing the
-        magazine, (3) packaging the magazine. Donations are not used toward any
-        form of compensation or unnecessary costs outside of the program budget.
-        We hope you consider giving.
-      </div>
-      {/* -------------------------------------------------------------------------------- */}
-      <div className="donateBottomTable">
-        {/* ---------------------------------------- */}
-        <div className="donateCostMenu">
-          <div className="donateItemTitle">
-            {/* Anhad Magazine <br></br>Costs Menu */}
-          </div>
-          <div className="donateItemChart">
-            <div className="donateItemItem">
-              {/* <div>ITEM</div> */}
-              {/* <p>01. ONE PRINTED MAGAZINE </p>
-              <p>02. ONE PRINTED MAGAZINE + DOMESTIC SHIPPING </p>
-              <p>
-                03. ONE PRINTED MAGAZINE + INTERNATIONAL SHIPPING <br></br>
-                [PRICE DEPENDS ON COUNTRY SELECTION]
-              </p>
-              <p>04. PACKAGING COSTS PER INDIVIDUAL </p> */}
-            </div>
+<div className="donateTitle">DONATE</div>
 
-            <div className="donateItemCost">
-              {/* <div>COST OF PRODUCTION </div> */}
-              {/* <p>$5.00</p>
-              <p>$8.00 +</p>
-              <p>$20.00 +</p>
-              <p>$5.00</p> */}
-            </div>
-          </div>
-        </div>
-        {/* ---------------------------------------- */}
-        <div className="donateColumn2">
-          {/* { <div className="venmo">VENMO US! @anhadmagazine</div>} */}
-          {/* ---------------------------------------- */}
-          {/* { <div className="dasvandh">Donate via Dasvandh Network! </div>} */}
-          {/* ---------------------------------------- */}
-          {/* {<div className="policy">Donor Policy and FAQs</div> } */}
-        </div>
-        {/* ---------------------------------------- */}
-      </div>
-      {/* -------------------------------------------------------------------------------- */}
-      <a
-        href="https://docs.google.com/forms/u/0/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <button> SUBMIT A REVIEW </button>
-      </a>
+{/* ---------------------------------------- */}
+
+<div id="line"className="hr1"><hr1 /></div>
+
+{/* ---------------------------------------- */}
+
+<div className="donateHeader1">Supporting ANHAD</div>
+<div className="donateDescription">
+  Anhad Magazine appreciates your support in any way possible. For us to
+  succeed in expanding our work and ensuring it is long-lasting, we need
+  your support. Your donation to Anhad will ensure Sikh literacy for ages
+  with guaranteed permanence and accessibility to the Sikh diaspora
+  through language and ownership of the magazine. All donations go
+  strictly toward: (1) promotion of the magazine, (2) printing the
+  magazine, (3) packaging the magazine. Donations are not used toward any
+  form of compensation or unnecessary costs outside of the program budget.
+  We hope you consider giving.
+</div>
+
+{/* ---------------------------------------- */}
+
+<div id="line"><hr2 /></div>
+
+{/* ---------------------------------------- */}
+
+<div className="donateHeader2">Your Donation will provide...</div>
+
+{/* -------------------------------------------------------------------------------- */}
+
+<div className="button">
+<a href="https://react.school" target="_blank" rel="noreferrer">
+  <button> $5 <br /> ONE PRINTED MAGAZINE </button>
+  <button> $5 <br /> ONE PRINTED MAGAZINE + DOMESTIC SHIPPING </button>
+  <button> $10 <br /> ONE PRINTED MAGAZINE + INTERNATIONAL SHIPPING <br />
+          [PRICE DEPENDS ON COUNTRY SELECTION] </button>
+  <button> $20 <br /> PACKAGING COSTS PER INDIVIDUAL </button>
+</a>
+</div>
+
+
     </div>
   );
 };
